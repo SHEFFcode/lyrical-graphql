1779f76d3f1e46b5723618ca08c1ca4030ac92ef
SHEFFmachine <sheff@sheffmachine.com>
2018-05-22 21:04:57 -0700

    we now have graphql hooked up to a component render

--- client/components/SongList.js
+++ client/components/SongList.js
@@ -1,13 +1,30 @@
 import React, { Component } from 'react';
+import gql from 'graphql-tag';
+import { graphql } from 'react-apollo';
 
 class SongList extends Component {
+  renderSongs() {
+    return this.props.data.songs.map(song => (
+      <li>{song.title}</li>
+    ));
+  }
+
   render() {
+
     return (
       <div>
-        SongList
+        {!this.props.data.loading && this.renderSongs()}
       </div>
     );
   }
 }
 
-export default SongList;
+const query = gql`
+  {
+    songs {
+      title
+    }
+  }
+`;
+
+export default graphql(query)(SongList);
