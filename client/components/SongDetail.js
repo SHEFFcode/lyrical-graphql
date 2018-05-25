c43c1ec43c999f415075ee24cd55dce80b446f04
SHEFFmachine <sheff@sheffmachine.com>
2018-05-25 17:35:18 -0500

    We now have a song list component that is able to query the DB with ids

--- client/components/SongDetail.js
+++ client/components/SongDetail.js
@@ -1,13 +1,26 @@
 import React, { Component } from 'react';
+import gql from 'graphql-tag';
+import { graphql } from 'react-apollo';
+import query from '../queries/fetchSong';
+import { Link } from 'react-router';
 
 class SongDetail extends Component {
   render() {
+    const { song } = this.props.data;
+    if (!song) {
+      return <div></div>;
+    }
     return (
       <div>
-        <h3>Song Details</h3>
+        <Link to="/">Back</Link>
+        <h3>{song.title}</h3>
       </div>
     );
   }
 }
 
-export default SongDetail;
+export default graphql(query, {
+  options(props) {
+    return { variables: { id: props.params.id } };
+  }
+})(SongDetail);
